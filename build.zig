95f00faf78643858465877c2eec48857a7ed779e
Rekihyt <checkmateguy@gmail.com>
2021-05-18 12:56:50 -0700

    change name to mymodule

--- build.zig
+++ build.zig
@@ -1,7 +1,7 @@
 const Builder = std.build.Builder;
 
 pub fn build(b: *Builder) void {
-    const obj = b.addObject("zig", "src/zig.zig");
+    const obj = b.addObject("mymodule", "src/mymodule.zig");
     const mode = b.standardReleaseOptions();
     obj.setBuildMode(mode);
     obj.addIncludeDir("include");
