c36e4d745d41589195f8b27c6cdc62129fb92f58
Rekihyt <checkmateguy@gmail.com>
2021-05-29 13:18:47 -0700

    test kprint using c std

--- src/kernel.zig
+++ src/kernel.zig
@@ -2,7 +2,31 @@ const std = @import("std");
 
 extern fn printk([*c]const u8) void;
 
+fn writeFn(context: void, bytes: []const u8) error{ }!usize {
+    printk(@ptrCast([*c]const u8, bytes));
+    return bytes.len;
+    // printk(&bytes);
+}
+const kWriter: std.io.Writer(void, error{ }, writeFn) = undefined;
+
 /// Formats the argument, then calls printk.
 pub fn print(comptime fmt: []const u8, args: anytype) void {
-    printk(std.fmt.comptimePrint(fmt, args));
+    std.fmt.format(kWriter, fmt, args) catch unreachable;
+}
+
+const printf = std.c.printf;
+const addNullByte = std.cstr.addNullByte;
+const allocator = std.testing.allocator;
+fn testPrint(v: void, str: []const u8) error{ OutOfMemory }!usize {
+    const cstr = try addNullByte(allocator, str);
+    allocator.free(cstr);
+    const len_or_err = printf("%*", &cstr);
+    return
+        if (len_or_err < 0) error.C
+        else @intCast(usize, );
+}
+test "writeFn" {
+    const testWriter: std.io.Writer(void, error{ OutOfMemory }, testPrint) = undefined;
+
+    try std.fmt.format(testWriter, "{s}\n", .{ "test" });
 }
