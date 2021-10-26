1fabaca9ed276df8989cf27ded31513493f02fd8
Rekihyt <checkmateguy@gmail.com>
2021-10-26 15:04:52 -0700

    update to 0.9.0 dev by removing usingnamespace

--- src/kernel.zig
+++ src/kernel.zig
@@ -2,12 +2,13 @@ const std = @import("std");
 
 extern fn printk([*c]const u8) void;
 
-fn writeFn(context: void, bytes: []const u8) error{ }!usize {
+fn writeFn(context: void, bytes: []const u8) error{}!usize {
     printk(@ptrCast([*c]const u8, bytes));
+    _ = context;
     return bytes.len;
     // printk(&bytes);
 }
-const kWriter: std.io.Writer(void, error{ }, writeFn) = undefined;
+const kWriter: std.io.Writer(void, error{}, writeFn) = undefined;
 
 /// Formats the argument, then calls printk.
 pub fn print(comptime fmt: []const u8, args: anytype) void {
@@ -17,16 +18,15 @@ pub fn print(comptime fmt: []const u8, args: anytype) void {
 const printf = std.c.printf;
 const addNullByte = std.cstr.addNullByte;
 const allocator = std.testing.allocator;
-fn testPrint(v: void, str: []const u8) error{ OutOfMemory }!usize {
+fn testPrint(v: void, str: []const u8) error{OutOfMemory}!usize {
+    _ = v;
     const cstr = try addNullByte(allocator, str);
     allocator.free(cstr);
     const len_or_err = printf("%*", &cstr);
-    return
-        if (len_or_err < 0) error.C
-        else @intCast(usize, );
+    return if (len_or_err < 0) error.C else @intCast(usize, len_or_err);
 }
 test "writeFn" {
-    const testWriter: std.io.Writer(void, error{ OutOfMemory }, testPrint) = undefined;
+    const testWriter: std.io.Writer(void, error{OutOfMemory}, testPrint) = undefined;
 
-    try std.fmt.format(testWriter, "{s}\n", .{ "test" });
+    try std.fmt.format(testWriter, "{s}\n", .{"test"});
 }
